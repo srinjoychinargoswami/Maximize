7a4a1666fb43fdee9e4b282da6db43b442a95916
Srinjoy Goswami <srinjoygoswami@gmail.com>
2025-07-05 23:08:47 -0400

    Modified some files in the plug ins to allow notifications to work

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
+#include <win_toast/win_toast_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   Sqlite3FlutterLibsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
+  WinToastPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WinToastPlugin"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   sqlite3_flutter_libs
+  win_toast
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
